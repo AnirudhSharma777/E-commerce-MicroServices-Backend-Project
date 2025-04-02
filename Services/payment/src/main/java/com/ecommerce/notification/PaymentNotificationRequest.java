3cf693a0500a997abfc34823b243f558391e1f86
Anirudh <97828169+AnirudhSharma777@users.noreply.github.com>
2025-04-02 19:02:23 +0530

    Payment Microservice

---
+++ Services/payment/src/main/java/com/ecommerce/notification/PaymentNotificationRequest.java
@@ -0,0 +1,21 @@
+package com.ecommerce.notification;
+
+import com.ecommerce.payment.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public record PaymentNotificationRequest(
+
+        String orderReference,
+
+        BigDecimal amount,
+
+        PaymentMethod paymentMethod,
+
+        String customerFirstName,
+
+        String customerLastName,
+
+        String customerEmail
+) {
+}
---
+++ Services/payment/src/main/java/com/ecommerce/payment/PaymentMapper.java
@@ -0,0 +1,16 @@
+package com.ecommerce.payment;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentMapper {
+
+    public Payment toPayment(PaymentRequest request) {
+        return Payment.builder()
+                .id(request.id())
+                .orderId(request.orderId())
+                .paymentMethod(request.paymentMethod())
+                .amount(request.amount())
+                .build();
+    }
+}
